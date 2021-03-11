4199397ecaa6d2df4605beb99b20e109f11f3044
Parth <parthparadkar3@gmail.com>
2021-03-11 11:00:52 +0530

    Fix imported models

--- drishtee/db/models/__init__.py
+++ drishtee/db/models/__init__.py
@@ -1,5 +1,1 @@
-from c3po.db.models.artist import Artist, ArtistGenre, ArtistSong
-from c3po.db.models.genre import Genre
-from c3po.db.models.link import Link
-from c3po.db.models.song import Song, SongGenre
-from c3po.db.models.user import User, UserLikes, UserPosts
+from drishtee.db.models.user import User
