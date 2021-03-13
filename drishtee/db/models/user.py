1c8c3c3c9a579a011fe1c62e9fbaf341f1166205
Parth <parthparadkar3@gmail.com>
2021-03-14 00:23:16 +0530

    Add user and bank models

--- drishtee/db/models/user.py
+++ drishtee/db/models/user.py
@@ -4,19 +4,34 @@ from sqlalchemy.schema import UniqueConstraint
 
 from drishtee.db.base import Base
 
-class User(Base):
-    __tablename__ = "user"
-    # Columns
+class UserSME(Base):
+    __tablename__ = "user_sme"
+    
     id = Column(Integer, primary_key=True)
+    created_at = Column("created_at", DateTime)
     name = Column("name", String(32), nullable=False)
-    facebook_id = Column("facebook_id", String, unique=True)
-    image = Column("image", String(160))
-    post_count = Column("post_count", Integer)
-    liked_count = Column("liked_count", Integer)
-    likes_count = Column("likes_count", Integer)
-    __table_args__ = (UniqueConstraint("id", "facebook_id", name="user_id"),)
+    username = Column("username", String(32), nullable=False)
+    phone = Column("phone", String(13))
+    industry_type = Column("industry_type", String(32))
+    bank_details_id = Column("bank_details_id", ForeignKey("User_bank_details.id"))
 
-    def __init__(self, name, facebook_id, image):
-        self.name = name
-        self.facebook_id = facebook_id
-        self.image = image
+    bank_details = relationship(
+        "BankDetails", backref=backref("user_sme", uselist=False)
+    )
+    
+class UserSHG(Base):
+    __tablename__ = "user_shg"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column("created_at", DateTime)
+    name = Column("name", String(32), nullable=False)
+    username = Column("username", String(32), nullable=False)
+    phone = Column("phone", String(13))
+    industry_type = Column("industry_type", String(32))
+    bank_details_id = Column("bank_details_id", ForeignKey("User_bank_details.id"))
+    prod_capacity = Column("prod_capacity", String(32))
+    order_size = Column("order_size", String(32))
+
+    bank_details = relationship(
+        "BankDetails", backref=backref("user_sme", uselist=False)
+    )
