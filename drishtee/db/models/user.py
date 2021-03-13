c7584fb8ee3f2c2559c39aedf4723541a054e3d2
Parth <parthparadkar3@gmail.com>
2021-03-14 00:31:09 +0530

    Add user dhg member model

--- drishtee/db/models/user.py
+++ drishtee/db/models/user.py
@@ -13,7 +13,7 @@ class UserSME(Base):
     username = Column("username", String(32), nullable=False)
     phone = Column("phone", String(13))
     industry_type = Column("industry_type", String(32))
-    bank_details_id = Column("bank_details_id", ForeignKey("User_bank_details.id"))
+    bank_details_id = Column("bank_details_id", ForeignKey("bank_details.id"))
 
     bank_details = relationship(
         "BankDetails", backref=backref("user_sme", uselist=False)
@@ -28,10 +28,24 @@ class UserSHG(Base):
     username = Column("username", String(32), nullable=False)
     phone = Column("phone", String(13))
     industry_type = Column("industry_type", String(32))
-    bank_details_id = Column("bank_details_id", ForeignKey("User_bank_details.id"))
+    bank_details_id = Column("bank_details_id", ForeignKey("bank_details.id"))
     prod_capacity = Column("prod_capacity", String(32))
     order_size = Column("order_size", String(32))
 
     bank_details = relationship(
         "BankDetails", backref=backref("user_sme", uselist=False)
+    )
+
+class UserSHGMember(Base):
+    __tablename__ = "user_shg_member"
+
+    id = Column(Integer, primary_key=True)
+    name = Column("name", String(32))
+    aadhar_details = Column("aadhar_details", String(32))
+    contact = Column("contact", String(32))
+    shg_id = Column("shg_id", ForeignKey("user_shg.id"))
+    skills = Column("skills", String(32))
+
+    shg = relationship(
+        "UserSHG", backref=backref("user_sme", uselist=False)
     )
