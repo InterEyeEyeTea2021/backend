495321cc5ad3fb39ca2b15757938d807dcdb1db5
Parth <parthparadkar3@gmail.com>
2021-03-14 22:34:45 +0530

    Add Bid table

--- drishtee/db/models/__init__.py
+++ drishtee/db/models/__init__.py
@@ -3,4 +3,4 @@ from drishtee.db.models.bank import BankDetails
 from drishtee.db.models.media import Media
 from drishtee.db.models.milestones import Milestone
 from drishtee.db.models.order import Order
-from drishtee.db.models.tender import Tender
+from drishtee.db.models.tender import Tender, Bid
