5cecacc41dafa6e9940c5855bbae1b868adf8947
Parth <parthparadkar3@gmail.com>
2021-03-15 22:57:24 +0530

    Unique bid validation

--- drishtee/api/service/bid_service.py
+++ drishtee/api/service/bid_service.py
@@ -17,17 +17,20 @@ class BidService:
     @staticmethod
     def create_bid(tender_id, amount, shg_id):
         with session_scope() as session:
-            shg = session.query(UserSHG).filter(UserSHG.id == shg_id).all()[0]
-            tender = session.query(Tender).filter(Tender.id == tender_id).all()[0]
-            new_bid = Bid(amount, shg, tender)
-            if new_bid:
-                session.add(new_bid)
-                response_data = {
-                    "success": True,
-                    "data": format_response(session, new_bid)
-                }
-                return response_data
-            return {"success": False}, 400
+            existing_bid = session.query(Bid).filter(Bid.shg_id == shg_id and Bid.tender_id == tender_id).all()
+            if not existing_bid:
+                shg = session.query(UserSHG).filter(UserSHG.id == shg_id).all()[0]
+                tender = session.query(Tender).filter(Tender.id == tender_id).all()[0]
+                new_bid = Bid(amount, shg, tender)
+                if new_bid:
+                    session.add(new_bid)
+                    response_data = {
+                        "success": True,
+                        "data": format_response(session, new_bid)
+                    }
+                    return response_data
+                return {"success": False, "message": "Bid creation failed!"}, 400
+            return {"success": False, "message": "Bid exists"}, 400 
 
     @staticmethod
     def get_tender_bids(tender_id):
