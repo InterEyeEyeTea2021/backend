970dd9690922551b1a0f9f9f5691730e722cb4c0
Parth <parthparadkar3@gmail.com>
2021-03-21 21:45:03 +0530

    Add milestone create and update endpoints

--- drishtee/api/controller/milestone_controller.py
+++ drishtee/api/controller/milestone_controller.py
@@ -16,7 +16,28 @@ class CompleteMilestone(Resource):
     @milestone_ns.expect(parser)
     def get(self):
         args = parser.parse_args()
-        print(args)
         if args["id"]:
             return MilestoneService.mark_completed(args["id"])
         return {"success": False}, 400
+
+@milestone_ns.route("/create")
+class CreateMilestone(Resource):
+    # parser = reqparse.RequestParser()
+    # parser.add_argument("id", type=int)
+    # @milestone_ns.expect(parser)
+    def post(self):
+        data = request.json
+        tender_id = data["tender_id"]
+        description = data["description"]
+        image_uri = data["image_uri"]
+        return MilestoneService.create_milestone(tender_id, description, image_uri)
+
+@milestone_ns.route("/update")
+class UpdateMilestone(Resource):
+    def post(self):
+        data = request.json
+        milestone_id = data.get("milestone_id")
+        description = data.get("description")
+        image_uri = data.get("image_uri")
+        print(milestone_id, description, image_uri)
+        return MilestoneService.update_milestone(milestone_id, description, image_uri)
--- drishtee/api/service/milestone_service.py
+++ drishtee/api/service/milestone_service.py
@@ -12,3 +12,22 @@ class MilestoneService:
         with session_scope() as session:
             milestone = session.query(models.Milestone).filter(models.Milestone.id == id_).update({models.Milestone.status: "completed"}, synchronize_session = False)
             return {"success": True}, 200
+
+    def create_milestone(tender_id, description, image_uri):
+        with session_scope() as session:
+            image = models.Media(image_uri, "image")
+            new_milestone = models.Milestone(description, "created", [image])
+            session.flush()
+            tender = session.query(models.Tender).filter(models.Tender.id == tender_id).first()
+            tender.milestones.append(new_milestone)
+            return {"success": True, "milestone_id": new_milestone.id}, 200
+
+    def update_milestone(milestone_id, description, image_uri):
+        with session_scope() as session:
+            if(description):
+                milestone = session.query(models.Milestone).filter(models.Milestone.id == milestone_id).update({models.Milestone.description: description})
+            if(image_uri):
+                milestone = session.query(models.Media).filter(models.Media.milestone_id == milestone_id).update({models.Media.uri: image_uri})
+            return {"success": True}, 200
+            
+
